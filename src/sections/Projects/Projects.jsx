a3617402f31a6f83534da45d8cac837916e9d6d7
sujitj50 <sujitkumarrjha@gmail.com>
2025-02-07 10:32:43 +0530

    adding new project in projecs page

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import Home from '../../assets/homePage.png'
 import Gallery from '../../assets/Gallery.jpeg'
 import ProjectCard from '../../common/ProjectCard';
 import estate from '../../assets/estate.jpeg'
+import course from '../../assets/course.jpeg'
 
 function Projects() {
   return (
@@ -13,6 +14,7 @@ function Projects() {
            <ProjectCard src={Home} link='https://github.com/itsuj/unis-social-client' h3='Unis' p='Social media app'/>
           <ProjectCard src={Gallery} link='https://gallery-app-frontend.onrender.com/' h3='Image App' p='Image Gallery Project'/>
           <ProjectCard src={estate} link='https://real-estate-page-mn25.onrender.com/' h3='Real estate' p='Real estate landing page'/>
+          <ProjectCard src={course} link='https://gencourses.netlify.app/' h3='Course Management' p='Course Mangement Web App'/>
         </div>
     </section>
   )
